edca4e05618adab5bf16068fad2595103868ab66
MSG8 <masogo008@gmail.com>
2021-11-11 08:29:58 +0100

    Expresiones  regulares

---
+++ regExp/js/validar.js
@@ -0,0 +1,34 @@
+/*
+  validar.css
+  Autor: Manuel Solis Gomez masogo008@gmail.com
+  Livencia: gp3
+*/
+'use strict'
+console.log('cargador');
+window.onload = iniciar;
+function iniciar() 
+{
+  document.getElementById('ip').onblur = comprobar;
+}
+function comprobar() 
+{
+  console.log('comprobar');
+  let valIp = new RegExp(/^(\d{1,3}\.){3}\d{1,3}$/);
+
+  if (document.getElementById('ip').value.match(valIp)) // Para el string
+  {
+    alert('valido');
+  }
+  else
+  {
+    alert('invalido');
+  }
+  // if (valIp.test(document.getElementById('ip').value))  //Para la ExpReg
+  // {
+  //   alert('valido');
+  // }
+  // else
+  // {
+  //   alert('invalido');
+  // }
+}
